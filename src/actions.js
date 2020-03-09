d44695da9617c83d36299d661419d8c2d4b6e798
Emmanuel <akhigbemmanuel@gmai.com>
2020-03-09 16:00:34 +0100

    CORS header

--- src/actions.js
+++ src/actions.js
@@ -9,14 +9,13 @@ export const setSearchField = text => ({
   type: CHANGE_SEARCH_FIELD,
   payload: text
 });
-
+let myHeaders = new Headers({
+  "Access-Control-Allow-Origin": "https://jsonplaceholder.typicode.com"
+});
 export const requestRobots = () => dispatch => {
   dispatch({ type: REQUEST_ROBOTS_PENDING });
   fetch("https://jsonplaceholder.typicode.com/users", {
-    mode: "cors",
-    headers: {
-      "Access-Control-Allow-Origin": "https://jsonplaceholder.typicode.com"
-    }
+    headers: myHeaders
   })
     .then(res => res.json())
     .then(res => dispatch({ type: REQUEST_ROBOTS_SUCCESS, payload: res }))
