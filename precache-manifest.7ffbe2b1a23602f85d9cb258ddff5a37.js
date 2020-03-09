9103f66ab5be32296f9d51f6684d1474611bd453
Emmanuel <akhigbemmanuel@gmai.com>
2020-03-09 14:19:51 +0100

    Updates

---
+++ precache-manifest.7ffbe2b1a23602f85d9cb258ddff5a37.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "984da91a7148006db697389d48979956",
+    "url": "/index.html"
+  },
+  {
+    "revision": "496cd0722a59da417039",
+    "url": "/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "07a0382d72d80a3f7198",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "496cd0722a59da417039",
+    "url": "/static/js/2.6b78c15d.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/static/js/2.6b78c15d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "07a0382d72d80a3f7198",
+    "url": "/static/js/main.ac6a71b5.chunk.js"
+  },
+  {
+    "revision": "7f31f04669a7e0c04c51",
+    "url": "/static/js/runtime-main.59d46d1a.js"
+  }
+]);
